4e2716e7d58ea2d748f8e9e99c917e3380027d8a
Diya Limbani <diyalimbani173@gmail.com>
2026-02-14 22:51:19 +0530

    Delete build files

--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/diya_limbani/FastDevFs/test
-# Build directory: /home/diya_limbani/FastDevFs/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
